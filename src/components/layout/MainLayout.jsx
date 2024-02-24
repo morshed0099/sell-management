a1ddcfd96388db4f1f7fea78e9b8ce1c616f9e7f
morshed0099 <mdgolammorshed0099@gmail.com>
2024-02-24 10:16:22 +0600

    tailwind setup and mainlayout page setup

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+const MainLayout = () => {
+  return (
+    <div className="flex gap-2">
+      <div className="flex-1 w-full bg-gray-700 h-screen">
+        <h1>hehe</h1>
+      </div>
+      <div className="flex-1 w-full bg-gray-600 h-screen">
+        <h1>hehe</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
