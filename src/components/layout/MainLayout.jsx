9c54d01f09d608dd078802202c092f19df95632a
morshed0099 <mdgolammorshed0099@gmail.com>
2024-02-24 10:26:07 +0600

    right side dware created

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,10 +1,13 @@
+import DrawerRightSide from "../ui/DrawerRightSide";
+
 const MainLayout = () => {
   return (
-    <div className="flex gap-2">
-      <div className="flex-1 w-full bg-gray-700 h-screen">
-        <h1>hehe</h1>
+    <div className="flex gap-2 relative">
+      <div className="flex-1 w-full  h-screen">
+        <DrawerRightSide />
+        <h1>hii</h1>
       </div>
-      <div className="flex-1 w-full bg-gray-600 h-screen">
+      <div className="flex-1 w-full  h-screen">
         <h1>hehe</h1>
       </div>
     </div>
---
+++ src/components/ui/DrawerRightSide.jsx
@@ -0,0 +1,11 @@
+const DrawerRightSide = () => {
+  return (
+    <div className="w-full h-screen absolute bg-gray-800 bg-opacity-25">
+      <div className="w-[200px] bg-white z-50 h-screen">
+        <h1>225</h1>
+      </div>
+    </div>
+  );
+};
+
+export default DrawerRightSide;
